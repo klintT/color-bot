212daa86fc841f3b4df96831e7987fdf7ba9ba88
Sara Thrasher <sarathrasher@saras-mbp.lan>
2024-01-19 16:20:45 -0500

    high alch working I think

--- src/model/osrs/high-alch.py
+++ src/model/osrs/high-alch.py
@@ -7,6 +7,7 @@ from model.osrs.osrs_bot import OSRSBot
 from utilities.api.morg_http_client import MorgHTTPSocket
 from utilities.api.status_socket import StatusSocket
 
+from utilities.methods.magic import highAlch
 
 class OSRSTemplate(OSRSBot):
     def __init__(self):
@@ -14,6 +15,7 @@ class OSRSTemplate(OSRSBot):
         description = "This high alchs items"
         super().__init__(bot_title=bot_title, description=description)
         self.running_time = 1
+        self.alch_items = []
 
     def create_options(self):
         """
@@ -35,38 +37,24 @@ class OSRSTemplate(OSRSBot):
             if option == "running_time":
                 self.running_time = options[option]
             elif option == "alch_items":
-                self.loot_items = options[option]
+                self.alch_items = options[option]
 
         self.log_msg(f"Running time: {self.running_time} minutes.")
-        self.log_msg(f'Loot items: {self.loot_items or "None"}.')
+        self.log_msg(f'Alch items: {self.alch_items or "None"}.')
         self.log_msg("Options set successfully.")
         self.options_set = True
 
     def main_loop(self):
-        """
-        When implementing this function, you have the following responsibilities:
-        1. If you need to halt the bot from within this function, call `self.stop()`. You'll want to do this
-           when the bot has made a mistake, gets stuck, or a condition is met that requires the bot to stop.
-        2. Frequently call self.update_progress() and self.log_msg() to send information to the UI.
-        3. At the end of the main loop, make sure to call `self.stop()`.
-
-        Additional notes:
-        - Make use of Bot/RuneLiteBot member functions. There are many functions to simplify various actions.
-          Visit the Wiki for more.
-        - Using the available APIs is highly recommended. Some of all of the API tools may be unavailable for
-          select private servers. For usage, uncomment the `api_m` and/or `api_s` lines below, and use the `.`
-          operator to access their functions.
-        """
         # Setup APIs
         # api_m = MorgHTTPSocket()
-        # api_s = StatusSocket()
+        api_s = StatusSocket()
 
         # Main loop
         start_time = time.time()
         end_time = self.running_time * 60
         while time.time() - start_time < end_time:
-            
-
+            for item in self.alch_items:
+                highAlch(self, item, api_s)
             self.update_progress((time.time() - start_time) / end_time)
 
         self.update_progress(1)
--- src/utilities/methods/magic.py
+++ src/utilities/methods/magic.py
@@ -1,22 +1,34 @@
-import pyautogui as pag
 import time
-import item_ids
+from status_socket import StatusSocket
+import utilities.api.item_ids_dict as item_dict
+import pytweening
+from utilities.random_util import truncated_normal_sample
 
-from model.runelite_bot import RuneLiteWindow
-
-def highAlch(self, item: str, count: int):
-    win: RuneLiteWindow = None
+def highAlch(self, item: str, api_s: StatusSocket):
+    id = item_dict[item.upper]
+    slots = api_s.get_inv_item_indices(id)
+    
     self.log_msg(f"High Alching Item: {item}...")
     self.mouse.move_to(self.win.cp_tab(6), mouseSpeed='medium')
-    pag.click()
+    self.mouse.click()
     time.sleep(0.5)
 
-    for i in range(0, count):
+    for slot in slots:
         self.mouse.move_to(self.win.spellbook_normal(36), mouseSpeed='medium')
-        pag.click()
-        time.sleep(.5)
+        self.mouse.click()
+        time.sleep(0.5)
 
-        #Find item in inv
-        id = item_ids[]
-
-    return True
+        p = self.win.inventory_slots[slot].random_point()
+        self.mouse.move_to(
+            (p[0], p[1]),
+            mouseSpeed="fastest",
+            knotsCount=1,
+            offsetBoundaryY=40,
+            offsetBoundaryX=40,
+            tween=pytweening.easeInOutQuad,
+        )
+        self.mouse.click()
+        LOWER_BOUND_CLICK = 3
+        UPPER_BOUND_CLICK = 5
+        AVERAGE_CLICK = 4
+        time.sleep(truncated_normal_sample(LOWER_BOUND_CLICK, UPPER_BOUND_CLICK, AVERAGE_CLICK))
